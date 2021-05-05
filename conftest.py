1473c810e9f85fcb9d42d9e1df7935a5d5042b15
Kelvin.Ye <testmankelvin@163.com>
2021-05-05 23:11:20 +0800

    🦄 refactor: conftest内容从conftests模块import

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @File    : __init__.py
+# @File    : conftest.py
 # @Time    : 2021/4/28 11:10
 # @Author  : Kelvin.Ye
-from .fixtures import *
+from conftests.fixtures.driver_fixture import *  # noqa
+from conftests.fixtures.variable_fixture import *  # noqa
+from conftests.plugins.html_plugin import *  # noqa
+from conftests.options import *  # noqa
